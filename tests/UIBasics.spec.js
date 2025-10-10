527608bd81a9e2c7603b55d0a16b84d42f0473b1
AmbarishDash15 <ambarish.amrit@gmail.com>
2025-10-11 08:24:32 +1100

    11-10 added TC

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,14 +1,36 @@
 const {test, expect} = require('@playwright/test')
 
 
-test('Browser Context Playwright Test',async ({browser}) => {
+test.skip('Browser Context Playwright Test',async ({browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto("http://www.rahulshettyacademy.com/loginpagePractise/")
+    await page.goto('http://www.rahulshettyacademy.com/loginpagePractise/');
 });
 
-test.only('Page Playwright Test',async ({page}) => {
-    await page.goto("https://www.google.com/")
+test.skip('Page Playwright Test',async ({page}) => {
+    await page.goto('https://www.google.com/');
     console.log(await page.title());
-    await expect(page).toHaveTitle('Google')
+    await expect(page).toHaveTitle('Google');
+});
+
+test('Validate Incorrect Login Error',async ({browser}) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('http://www.rahulshettyacademy.com/loginpagePractise/');
+    await page.locator('#username').fill('rahulshetty');
+    await page.locator('[name="password"]').fill('learning');
+    await page.locator('#signInBtn').click();
+    console.log(await page.locator('[style*="block"]').textContent());
+    expect(await page.locator('[style*="block"]').textContent()).toContain('Incorrect username/password.');
+});
+
+test.only('Validate Empty User Name Login Error',async ({browser}) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('http://www.rahulshettyacademy.com/loginpagePractise/');
+    await page.locator('#username').fill('');
+    await page.locator('[name="password"]').fill('learning');
+    await page.locator('#signInBtn').click();
+    console.log(await page.locator('[style*="block"]').textContent());
+    expect(await page.locator('[style*="block"]').textContent()).toContain('Empty username/password.');
 });
